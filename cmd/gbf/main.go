e40575bbfecffdb6717b9ba275cc47fb53f93e66
Christopher Kleine <c.kleine@fischersys.de>
2022-09-29 15:13:45 +0200

    Embedded test test files

--- cmd/gbf/main.go
+++ cmd/gbf/main.go
@@ -1,27 +1,63 @@
 package main
 
 import (
+	"embed"
 	"flag"
 	"fmt"
 	"gbf"
 	"gbf/memory"
 	"os"
+	"strings"
 )
 
-var f string
+var (
+	exFile       string
+	emFile       string
+	listEmbedded bool
+)
+
+//go:embed testing/*.b
+var files embed.FS
 
 func main() {
-	flag.StringVar(&f, "f", "", "Filename to run")
+	flag.StringVar(&exFile, "f", "", "Filename to run")
+	flag.StringVar(&emFile, "e", "", "Name of embedded file")
+	flag.BoolVar(&listEmbedded, "l", false, "List embedded bf files")
 	flag.Parse()
 
-	if f == "" {
-		fmt.Println("input file required")
-		os.Exit(1)
+	var (
+		err  error
+		data []byte
+	)
+
+	if exFile != "" {
+		data, err = os.ReadFile(exFile)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	} else if emFile != "" {
+		data, err = files.ReadFile("testing/" + emFile + ".b")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	} else if listEmbedded {
+		fileList, err := files.ReadDir("testing")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		for _, f := range fileList {
+			fmt.Println(strings.TrimSuffix(f.Name(), ".b"))
+		}
+
+		os.Exit(0)
 	}
 
-	data, err := os.ReadFile(f)
-	if err != nil {
-		fmt.Println(err)
+	if len(data) == 0 {
+		fmt.Println("No bf file provided. Use -f to specify a filename, -e to use an embedded file or -l to list the embedded files.")
 		os.Exit(1)
 	}
 
